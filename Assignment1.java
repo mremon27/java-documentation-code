50a0f40a49ef8d04154d211275bd649aab31ca58
mremon27 <mremonkhan10101@gmail.com>
2024-11-28 13:10:52 +0600

    Update Assignment1.java. emon

--- Assignment1.java
+++ Assignment1.java
@@ -1,13 +1,8 @@
-/*
- * Assignment-1
- * create a class called Product
- * create a main method
- * print the following data in main method
-
-
- id: 101,
-title: iphone15,
-price: 1895 euros,
-description: perfect product with best image quality,
-category: phone,
- */
+Assignment - 1
+ class called Product{
+ public static void main(String[]args){
+ System.out.println("id:100");
+ System.out.println("title: iphone15");
+ System.out.println("price:1895 euros");
+ System.out.println("description: perfect with best image quality");
+ System.out.println("category:phone");
